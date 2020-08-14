2216f45c4fd50dd14fececae86ddeb69d3193190
aokidaisuke-a <69652211+aokidaisuke-a@users.noreply.github.com>
2020-08-14 15:01:09 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -4,6 +4,22 @@ from datetime import datetime
 
 client = discord.Client()
 
+# 起動時に動作する処理
+@client.event
+async def on_ready():
+    # 起動したらターミナルにログイン通知が表示される
+    print('ログインしました')
+
+# メッセージ受信時に動作する処理
+@client.event
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    # 「/neko」と発言したら「にゃーん」が返る処理
+    if message.content == '/neko':
+        await message.channel.send('にゃーん'
+
 @client.event
 async def on_message(message):
     if message.author.id == 159985870458322944: # MEE6からのメッセージかどうかを判別
