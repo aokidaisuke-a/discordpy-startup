6af18e08f52093b5cae4fba7599956bd792fb8a6
aokidaisuke-a <69652211+aokidaisuke-a@users.noreply.github.com>
2020-08-14 14:57:07 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -32,16 +32,22 @@ async def on_message(message):
                 await target.add_roles(levelrole5)
                 await target.remove_roles(levelrole1)
                 
-            elif level == 6: # レベル5になった時の処理
-                levelrole1 = discord.utils.get(message.server.roles, name="チンカス")
+            elif level == 6: # レベル6になった時の処理
+                levelrole5 = discord.utils.get(message.server.roles, name="初珍者")
                 levelrole6 = discord.utils.get(message.server.roles, name="珍参者")
                 await target.add_roles(levelrole6)
-                await target.remove_roles(levelrole1)
+                await target.remove_roles(levelrole5)
 
-            elif level == 7: # レベル5になった時の処理
-                levelrole1 = discord.utils.get(message.server.roles, name="チンカス")
+            elif level == 7: # レベル7になった時の処理
+                levelrole6 = discord.utils.get(message.server.roles, name="珍参者")
                 levelrole7 = discord.utils.get(message.server.roles, name="珍遊詩人")
                 await target.add_roles(levelrole7)
+                await target.remove_roles(levelrole6)
+                
+           elif level == 8: # レベル8になった時の処理
+                levelrole1 = discord.utils.get(message.server.roles, name="チンカス")
+                levelrole8 = discord.utils.get(message.server.roles, name="珍者")
+                await target.add_roles(levelrole8)
                 await target.remove_roles(levelrole1)
 
 client.run("NzQzNDE3ODgxNzEwMTY2MDk3.XzUX2w.-P6BKZkVVqTPV3ZrqBkwlwPs-sc") # Botのトークン
