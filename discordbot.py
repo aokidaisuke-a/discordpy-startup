462164964dd82ac880af2f8dfbaa348efadf4ec4
aokidaisuke-a <69652211+aokidaisuke-a@users.noreply.github.com>
2020-08-14 15:37:56 +0900

    Add files via upload

--- discordbot.py
+++ discordbot.py
@@ -1,23 +1,27 @@
+# インストールした discord.py を読み込む
 import discord
 
+# 自分のBotのアクセストークンに置き換えてください
+TOKEN = 'DISCORD_BOT_TOKEN'
+
+# 接続に必要なオブジェクトを生成
 client = discord.Client()
 
+# 起動時に動作する処理
 @client.event
 async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
+    # 起動したらターミナルにログイン通知が表示される
+    print('ログインしました')
 
+# メッセージ受信時に動作する処理
 @client.event
 async def on_message(message):
-    # 「おはよう」で始まるか調べる
-    if message.content.startswith("おはよう"):
-        # 送り主がBotだった場合反応したくないので
-        if client.user != message.author:
-            # メッセージを書きます
-            m = "おはようございます" + message.author.name + "さん！"
-            # メッセージが送られてきたチャンネルへメッセージを送ります
-            await message.channel.send(m)
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    # 「/neko」と発言したら「にゃーん」が返る処理
+    if message.content == '/neko':
+        await message.channel.send('にゃーん')
 
-client.run("DISCORD_BOT_TOKEN") # Botのトークン
+# Botの起動とDiscordサーバーへの接続
+client.run(TOKEN)
